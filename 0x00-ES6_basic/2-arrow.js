941fafe6a7a019dee9cd527e8794c0ebfc67fb84
Bishop121 <obidikeemmanuel@outlook.com>
2024-10-10 17:22:35 +0100

    Arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -5,11 +5,4 @@ export default function getNeighborhoodsList() {
 		          this.sanFranciscoNeighborhoods.push(newNeighborhood);
 		          return this.sanFranciscoNeighborhoods;
 		        };
-}export default function getNeighborhoodsList() {
-	    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-	  
-	    this.addNeighborhood = (newNeighborhood) => {
-		          this.sanFranciscoNeighborhoods.push(newNeighborhood);
-		          return this.sanFranciscoNeighborhoods;
-		        };
 }
