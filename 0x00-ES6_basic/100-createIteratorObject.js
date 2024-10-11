b3e6ab5b9a1d0f886e027b5550deb8c2021507bd
Bishop121 <obidikeemmanuel@outlook.com>
2024-10-11 16:16:25 +0100

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,25 +1,13 @@
 export default function createIteratorObject(report) {
-	  const { allEmployees } = report;
+  const { allEmployees } = report;
 
-	  function* employeeGenerator() {
-		      for (const department of Object.values(allEmployees)) {
-			            for (const employee of department) {
-					            yield employee;
-					          }
-			          }
-		    }
+  function* employeeGenerator() {
+    for (const department of Object.values(allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }
 
-	  return employeeGenerator();
-}export default function createIteratorObject(report) {
-	  const { allEmployees } = report;
-
-	  function* employeeGenerator() {
-		      for (const department of Object.values(allEmployees)) {
-			            for (const employee of department) {
-					            yield employee;
-					          }
-			          }
-		    }
-
-	  return employeeGenerator();
+  return employeeGenerator();
 }
