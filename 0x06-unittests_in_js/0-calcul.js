b46a7759be861b92934d98621d3dbc0b8ec29395
Bishop121 <obidikeemmanuel@outlook.com>
2024-11-29 09:50:31 -0800

    jane

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+function calculateNumber(a, b) {
+  // Round the numbers using Math.round() and return the sum
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/test/test.js
@@ -0,0 +1,9 @@
+// test/test.js
+const assert = require('assert');
+
+describe('Basic Math Test', function() {
+  it('should return 4 when adding 2 + 2', function() {
+    assert.strictEqual(2 + 2, 4);
+  });
+});
+
